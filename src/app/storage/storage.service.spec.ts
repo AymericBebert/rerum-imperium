86a39029a754eed64bbeb5adfcece0c7d9f25a11
AymericBebert <aymeric.bernard@hotmail.fr>
2022-03-06 19:16:00 +0100

    fix removal of CookieService

--- src/app/storage/storage.service.spec.ts
+++ src/app/storage/storage.service.spec.ts
@@ -1,12 +1,10 @@
 import {TestBed} from '@angular/core/testing';
-import {CookieService} from 'ngx-cookie-service';
 import {StorageService} from './storage.service';
 
 describe('StorageService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
       StorageService,
-      CookieService,
     ],
   }));
 

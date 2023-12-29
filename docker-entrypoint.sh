de4c5de96f2731503599323e00470c8363d80118
AymericBebert <aymeric.bernard@hotmail.fr>
2023-12-29 17:04:25 +0100

    chore(build): fix checksums in ngsw.json

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,11 +21,17 @@ CONFIG_BLOCK="<!--APP_CONFIG INSERTED-->
 <script>
 $(cat /config.js)
 </script>"
+INDEX_SHA1_OLD=$(sha1sum /usr/share/nginx/html/index.html | cut -d '  ' -f 1)
 # shellcheck disable=SC2005
 echo "$(awk -v r="$CONFIG_BLOCK" '{gsub(/<!--APP_CONFIG-->/,r)}1' /usr/share/nginx/html/index.html)" >/usr/share/nginx/html/index.html
+INDEX_SHA1_NEW=$(sha1sum /usr/share/nginx/html/index.html | cut -d '  ' -f 1)
+sed -i "s/\"$INDEX_SHA1_OLD\"/\"$INDEX_SHA1_NEW\"/" /usr/share/nginx/html/ngsw.json
 
 if [ -n "${APP_SHORT_NAME:-}" ]; then
+  MANIFEST_SHA1_OLD=$(sha1sum /usr/share/nginx/html/manifest.webmanifest | cut -d '  ' -f 1)
   sed -i "s/\"short_name\": \"Rerum\"/\"short_name\": \"$APP_SHORT_NAME\"/" /usr/share/nginx/html/manifest.webmanifest
+  MANIFEST_SHA1_NEW=$(sha1sum /usr/share/nginx/html/manifest.webmanifest | cut -d '  ' -f 1)
+  sed -i "s/\"$MANIFEST_SHA1_OLD\"/\"$MANIFEST_SHA1_NEW\"/" /usr/share/nginx/html/ngsw.json
 fi
 
 exec "$@"
